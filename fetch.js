5771d8adcf70268fc42f0ec553782ac01aeeb584
Ed Summers <ehs@pobox.com>
2015-12-04 16:07:20 +0000

    more

--- fetch.js
+++ fetch.js
@@ -1,4 +1,5 @@
 var page = require('webpage').create();
+page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36';
 
 var filename = Date.now() + ".png";
 
