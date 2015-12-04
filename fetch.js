ced913ca106aed0220cf4e3adacd33963515cda4
Ed Summers <ehs@pobox.com>
2015-12-04 15:55:22 +0000

    trying  a pdf

--- fetch.js
+++ fetch.js
@@ -1,7 +1,7 @@
 var page = require('webpage').create();
 page.settings.resourceTimeout = 3000;
 
-var filename = Date.now() + ".png";
+var filename = Date.now() + ".pdf";
 
 page.open('http://www.internetannouncementboard.com/', function(status) {
     page.render(filename);
