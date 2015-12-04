0e532081208f2a9b0a5d9a283909aa20468b7b19
Ed Summers <ehs@pobox.com>
2015-12-04 16:02:43 +0000

    more

--- fetch.js
+++ fetch.js
@@ -3,6 +3,7 @@ var page = require('webpage').create();
 var filename = Date.now() + ".png";
 
 page.open('http://www.internetannouncementboard.com/', function(status) {
+    console.log(status);
     page.render(filename);
     phantom.exit();
 });
