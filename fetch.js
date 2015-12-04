f570768528131ae853b5da8096366ec71c205c96
Ed Summers <ehs@pobox.com>
2015-12-04 16:04:56 +0000

    ugh

--- fetch.js
+++ fetch.js
@@ -1,8 +1,9 @@
 var page = require('webpage').create();
 
-var filename = Date.now() + ".jpg";
+var filename = Date.now() + ".png";
 
-page.open('http://www.internetannouncementboard.com/', function(status) {
+//page.open('http://www.internetannouncementboard.com/', function(status) {
+page.open('http://inkdroid.org/', function(status) {
     console.log(status);
     page.render(filename);
     phantom.exit();
